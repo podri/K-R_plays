858204a892ac72a9d4ce5c0b990214a20ad5feba
Podri <podrigabor@gmail.com>
2010-10-31 22:45:31 +0100

    playing with shift operator

--- kr2-1.c
+++ kr2-1.c
@@ -4,3 +4,28 @@
  *and by direct computation. Harder if you compute them: determine the ranges of the various
  *floating-point types.
 */
+
+#include <stdio.h>
+#include <limits.h>
+
+int main(){
+	printf("limits from header:\n");
+	printf("SCHAR_MIN: %d\n", SCHAR_MIN);
+	printf("SCHAR_MAX: %d\n", SCHAR_MAX);
+	printf("UCHAR_MAX: %u\n", UCHAR_MAX);
+	printf("CHAR_MIN: %d\n", CHAR_MIN);
+	printf("CHAR_MAX: %d\n", CHAR_MAX);
+	printf("SHRT_MIN: %d\n", SHRT_MIN);
+	printf("SHRT_MAX: %d\n", SHRT_MAX);
+	printf("USHRT_MAX: %u\n", USHRT_MAX);
+	printf("INT_MIN: %d\n", INT_MIN);
+	printf("INT_MAX: %d\n", INT_MAX);
+	printf("UINT_MAX: %u\n", UINT_MAX);
+	printf("LONG_MIN: %ld\n", LONG_MIN);
+	printf("LONG_MAX: %ld\n", LONG_MAX);
+	printf("ULONG_MAX: %lu\n", ULONG_MAX);
+
+	return 0;
+
+}
+
